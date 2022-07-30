1ecbff85c62a7fa19a9250df64f9d181a578374f
alessio <alessio5263@gmail.com>
2022-07-30 20:24:26 +0200

    es-15

---
+++ 15 - Closures/README.md
@@ -0,0 +1,6 @@
+# Closure - Exercise 2
+
+Implementare la funzione `pow` che accetta come argomento la base (`base`) e restituisce una closure che si occupa di elevare a potenza la base per l'esponente passato come parametro
+
+Per usare le potenze in Javascript, puoi usare il metodo Math.pow(), oppure l'operatore **
+
---
+++ 15 - Closures/exercise-2.js
@@ -0,0 +1,19 @@
+function pow(base) {
+  return function (numbers) {
+    return base ** numbers
+
+  }
+}
+
+const pow2 = pow(2);
+const pow5 = pow(5);
+
+console.log('--- Power of 2 ---');
+console.log(pow2(4));
+console.log(pow2(2));
+console.log(pow2(8));
+
+console.log('--- Power of 5 ---');
+console.log(pow5(4));
+console.log(pow5(2));
+console.log(pow5(8));
