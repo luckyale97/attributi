ac51752bb8275cb99cce6854a1a4c9464e57bf3d
alessio <alessio5263@gmail.com>
2022-08-14 21:13:10 +0200

    es-27

--- 26 - Costruttori/exercise-2.js
+++ 26 - Costruttori/exercise-2.js
@@ -1,6 +1,6 @@
 function Person(firstName, lastName) {
-  this.firstName= firstName;
-  this.lastName= lastName;
+  this.firstName = firstName;
+  this.lastName = lastName;
   fullName = () => `${this.firstName} ${this.lastName}`
 }
 
@@ -9,5 +9,6 @@ const simon = new Person("Simon", "Collins");
 
 
 
-console.log(john.fullName()); // John Doe
-console.log(simon.fullName()); // Simon Collins
+console.log(this.fullName()); // John Doe
+console.log(this.fullName()); // Simon Collins
+
