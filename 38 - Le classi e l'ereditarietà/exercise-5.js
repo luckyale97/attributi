913613f679bce15447d619ca37244ad43a9797ac
alessio <alessio5263@gmail.com>
2022-08-27 17:21:42 +0200

    es-38

---
+++ 38 - Le classi e l'ereditarietà/exercise-5.js
@@ -0,0 +1,47 @@
+class BankAccount {
+  #amount = 0;
+  
+    constructor(initialAmount) {
+      this.#amount = initialAmount;
+    }
+  
+    deposit(amount) {
+      this.#amount += amount;
+    }
+  
+    withdraw(amount) {
+      this.#amount -= amount;
+    }
+  
+    view() {
+      console.log(this.#amount);
+    }
+  }
+  
+  class BankAccountVip extends BankAccount {
+  
+    constructor(initialAmount) {
+      super(initialAmount)
+    }
+  
+    deposit(amount) {
+      if (amount >= 1000) {
+        super.deposit(this.int(amount))   
+      } else {
+        super.deposit(amount)             
+      }
+    }
+  
+    int(amount){       
+     return amount * 1.03;  
+    }
+  
+  
+  }
+  
+  const bankAccountVip = new BankAccountVip(1000);
+  bankAccountVip.deposit(500);
+  bankAccountVip.deposit(1200);
+  bankAccountVip.withdraw(800);
+  bankAccountVip.deposit(3500);  
+  bankAccountVip.view();      
